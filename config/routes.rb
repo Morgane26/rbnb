b9fedfc6e8783831cb0e90b7931b49432c356bd7
elise serres <eliseserres@hotmail.fr>
2018-05-16 12:55:10 +0200

    cheee pas

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  resources :cocktails, only: [:index, :show, :new, :create] do
+  resources :doses, only: [:new, :create, :destroy], shallow: true
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
