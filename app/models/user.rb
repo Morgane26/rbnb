a5fa752ab707ad4879066f092ebfe592115fcea3
elise serres <eliseserres@hotmail.fr>
2018-05-16 15:40:37 +0200

    creation de seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :orders
-  has_many :meals
+  has_many :orders, dependent: :destroy
+  has_many :meals, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.all.destroy_all
+
+user1 = User.create!(email: "titi@hotmail.fr", name: "Titi", password: "password")
+#faker 10 utilisateur fake
+10.times do
+  Meal.create!(
+    user: User.all.sample,
+    name: Faker::HowIMetYourMother.character,
+    description: Faker::HowIMetYourMother.quote,
+    calories: Faker::Number.decimal(2),
+    price: Faker::Number.number(2),
+    date: Faker::Date.forward(14),
+    stock: Faker::Number.decimal(2),
+  )
+end
