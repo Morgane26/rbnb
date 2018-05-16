8657ce5cbf3cf5f9e648b3c0888b5ba671f94a3e
elise serres <eliseserres@hotmail.fr>
2018-05-16 12:57:17 +0200

    cheee pas

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :cocktails, only: [:index, :show, :new, :create] do
-  resources :doses, only: [:new, :create, :destroy], shallow: true
-
 
   resources :meals
   resources :orders
